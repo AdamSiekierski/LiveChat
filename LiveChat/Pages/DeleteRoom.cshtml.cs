612446b6e5875c6593f83bcdded4f2d0af0b0d48
macbook <macbook@MacBook-Pro-macbook.local>
2023-03-18 18:03:10 +0100

    room deleting

---
+++ LiveChat/Pages/DeleteRoom.cshtml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LiveChat.Data;
+using LiveChat.Models;
+
+namespace LiveChat.Pages
+{
+    public class DeleteRoomModel : PageModel
+    {
+        private readonly LiveChat.Data.ApplicationDbContext _context;
+
+        public DeleteRoomModel(LiveChat.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Room Room { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Rooms == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Room = room;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Rooms == null)
+            {
+                return NotFound();
+            }
+            var room = await _context.Rooms.FindAsync(id);
+
+            if (room != null)
+            {
+                Room = room;
+                _context.Rooms.Remove(Room);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
